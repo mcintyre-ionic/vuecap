66f30831974b110083076c8eae790931b6df6c8d
kimberly McIntyre <kimberlymcintyre@kimberlys-MacBook-Air.local>
2022-02-25 15:15:41 -0500

    Updated cap 3 packages. Replace config file with ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'io.ionic.starter',
+    appName: 'vuecap',
+    webDir: 'dist',
+    plugins: {},
+};
+
+export default config;
